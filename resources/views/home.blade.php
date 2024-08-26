b311aa3c0d9988c479f1768a0d3b41502e01b527
Niway Chemere <cniway94@gmail.com>
2024-08-25 20:17:59 -0900

    the day 6 commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,5 +3,11 @@
         Home Page
     </x-slot:heading>
 
-    <h1> {{$greeting}}. from the Home Page. My name is {{$name}}.</h1>
+    
+
+        @foreach ($jobs as $job )
+        <li> The job <strong> {{$job ['title']}} </strong> with ID <strong>{{$job ['id']}}</strong> pays <strong>{{$job['salary']}}</strong> per year. </li>
+        @endforeach
+
+    
 </x-layout>
