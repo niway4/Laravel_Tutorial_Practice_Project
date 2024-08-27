bfd5d9d5cc5851221e2fabbaa86f2c521802a28d
Niway Chemere <cniway94@gmail.com>
2024-08-26 21:53:33 -0900

    day 7 last commmit

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -1,9 +1,17 @@
+@php
+
+use App\Models\Job;
+
+@endphp
+
+
+
 <x-layout>
     <x-slot:heading>
         Jobs Page
     </x-slot:heading>
 
-    @foreach ($jobs1234 as $job)
+    @foreach (Job::all() as $job)
         <li>
             <a href="/jobs/{{ $job['id'] }}" class="hover:underline text-blue-500">
                 <strong>{{ $job['title'] }}:</strong> Pays {{ $job['salary'] }} per year.
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
+use App\Models\job;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');    
+    return view('home');
 });
 
 Route::get('/contact', function () {
@@ -11,50 +12,17 @@ Route::get('/contact', function () {
 });
 
 Route::get('/jobs', function () {
-    return view('jobs',
-    [   'jobs1234' => [
+    return view(
+        'jobs',
         [
-            'id' => 1,
-            'title' => 'Director',
-            'salary' => '$50,000'
-        ],
-        [
-            'id' => 2,
-            'title' => 'Programmer',
-            'salary' => '$10,000'
-        ],
-        [
-            'id' => 3,
-            'title' => 'Teacher',
-            'salary' => '$40,000'
+            'jobs' => Job::all()
         ]
-    ]
-     ]
-);
+    );
 });
 
 Route::get('/jobs/{id}', function ($id) {
-    $jobs1234 = [
-        [
-            'id' => 1,
-            'title' => 'Director',
-            'salary' => '$50,000'
-        ],
-        [
-            'id' => 2,
-            'title' => 'Programmer',
-            'salary' => '$10,000'
-        ],
-        [
-            'id' => 3,
-            'title' => 'Teacher',
-            'salary' => '$40,000'
-        ]
-    ];
-    $defaultUser = ['id' => 0, 'name' => 'Guest'];
-    $job = Arr::first($jobs1234, fn($job) => $job['id'] == $id, $defaultUser) ;
-    //  dump($job); // you can see the difference between dump() and dd() function.
-    //  echo 'My name is Niway Chemere';
+    $job = Job::find($id);
     return view('indivjob', ['work' => $job]);
 });
+
 
